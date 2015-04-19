54e66d23db8cb0c5a09681150fc617fc45a50114
Michael-Earley <michael.earley@me.com>
2015-04-19 16:32:28 +0100

    Publish April project
    April Project

--- Project 3 - April/scripts/april.js
+++ Project 3 - April/scripts/april.js
@@ -1,6 +1,27 @@
 // stuff that happens when all the elements on the page are loaded in the browser
 $(document).ready(function () {
 
-
+	// audio player and source
+	var audioPlayer = $("#audioPlayer");
+	var audioSource = $("#audioSource");
+	
+	// folder where images are located
+	var folder = "sounds/";
+	var audioSelection = "";
+	
+	// load the current selection
+	audioSelection = folder + $("#audioSelection option:selected").val();
+	audioSource.attr('src', audioSelection);
+    audioPlayer.load();
+    
+	
+	// change the sound selection
+    $("#audioSelection").change(function () {
+    
+        audioSelection = folder + $("#audioSelection option:selected").val();
+        audioSource.attr('src', audioSelection);
+        audioPlayer.load(); 
+        
+    });
 
 });
